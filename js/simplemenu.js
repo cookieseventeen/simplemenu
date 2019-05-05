ae914586f7933082f9f12a7b4c37cdaf3a4bc1ad
謝秉錡 <cookie_seventeen@msn.com>
2019-05-05 18:01:54 +0800

    新增功能 1.預設展開, 2.客制展開按鈕, 3.修改隱藏按鈕的方式

--- js/simplemenu.js
+++ js/simplemenu.js
@@ -4,21 +4,28 @@ $.fn.simplemenu = function(options) {
   var settings = $.extend(
     {
       usingbtn: true,
+      directorylink: true,
       custombtnname: "openmenu",
-      directorylink: true
+      currentcss:'active',
+      autoopen:true,
+      customicon:'<div class="simplemenu-normal-icon"><span></span><span></span></div>'
     },
     options
   );
 
   var btnclassname = settings.custombtnname,
-    directorylink = settings.directorylink,
-    usingbtn = settings.usingbtn;
+      directorylink = settings.directorylink,
+      usingbtn = settings.usingbtn,
+      currentcss = settings.currentcss,
+      autoopen= settings.autoopen,
+      customicon= settings.customicon;
+
   
   this.find("a").each(function() {
     //build icon
     if ($(this).siblings("ul").length > 0) {
       $(this)
-        .append('<span class="' + btnclassname + '"></span>')
+        .append('<span class="' + btnclassname + '">'+customicon+'</span>')
         .parent()
         .addClass("has-submenu");
     }
@@ -32,14 +39,14 @@ $.fn.simplemenu = function(options) {
         
         var _this_parent_li=$(this).parent('a').parent('li');
       
-        if(_this_parent_li.hasClass('active')){
+        if(_this_parent_li.hasClass(currentcss)){
           //如果已經展開 轉回關閉 連同子選單一起
-          _this_parent_li.removeClass('active').children('ul').slideUp().find('.active').removeClass('active').children('ul').slideUp();
+          _this_parent_li.removeClass(currentcss).children('ul').slideUp().find('.'+currentcss).removeClass(currentcss).children('ul').slideUp();
         }else{
           //如果還沒展開 展開這一層
-          _this_parent_li.addClass('active').children('ul').slideDown();
+          _this_parent_li.addClass(currentcss).children('ul').slideDown();
           //關閉其他階層的選單
-          _this_parent_li.siblings().removeClass('active').children('ul').slideUp().find('.active').removeClass('active').children('ul').slideUp();
+          _this_parent_li.siblings().removeClass(currentcss).children('ul').slideUp().find('.'+currentcss).removeClass(currentcss).children('ul').slideUp();
         }  
       });
   });
@@ -49,6 +56,11 @@ $.fn.simplemenu = function(options) {
   }
   if (!directorylink) {
     this.find(".has-submenu").addClass("nodirectorylink");
+  }
+  
+  //載入時預設展開currentcss下的ul
+  if(autoopen){
+    this.find('.'+currentcss).children('ul').slideDown();
   }
 
   // return this;
